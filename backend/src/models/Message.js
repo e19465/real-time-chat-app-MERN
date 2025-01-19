0d0760d90b097c9bbe5f0cb8e3cb98ab2c89adcd
Sasindu Dilshan Rathnayaka <sasindudil0002@gmail.com>
2025-01-20 00:47:56 +0530

    added timestamps in message modal

--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -1,23 +1,26 @@
 const mongoose = require("mongoose");
 const { ModalNames } = require("../constants/common");
 
-const messageSchema = new mongoose.Schema({
-  senderId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: ModalNames.User,
-    required: true,
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: ModalNames.User,
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: ModalNames.User,
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
   },
-  receiverId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: ModalNames.User,
-    required: true,
-  },
-  text: {
-    type: String,
-  },
-  image: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Message", messageSchema);
