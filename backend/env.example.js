aec792916438b1bf4ed5c67c4a5c86e1ed1aa7c0
Sasindu Dilshan Rathnayaka <sasindudil0002@gmail.com>
2025-01-29 00:37:28 +0530

    final touches -> V1 finished

--- backend/env.example.js
+++ backend/env.example.js
@@ -1,12 +1,32 @@
-const MONGO_URI = ""; // MongoDB URI
-const ALLOWED_ORIGINS = "http://localhost:5173,http://localhost:5000"; // allowed origins for CORS
-const PORT = 5000; // port for the server
-const ACCESS_TOKEN_SECRET = ""; // secret for access token
-const REFRESH_TOKEN_SECRET = ""; // secret for refresh token
-const SD_CHAT_SUPPORT_EMAIL = ""; // email for support
-const APP_EMAIL = ""; // email for sending emails to users
-const APP_EMAIL_PASSWORD = ""; // password for the email
-const NODE_ENV = "development"; // if in production, set to "production"
-const CLOUDINARY_CLOUD_NAME = ""; // cloudinary cloud name
-const CLOUDINARY_API_KEY = ""; // cloudinary api key
-const CLOUDINARY_API_SECRET = ""; // cloudinary api secret
+//! >> Define all environment variables in .env file in local development << !//
+//! >> Supply these all environment variables in production environment << !//
+
+// Databse URI
+const MONGODB_CONNECTION_URI =
+  "mongodb+srv://sasindudil0002:xrIktyXOrrFcDhZj@chat-mern.8z2th.mongodb.net/chat_db?retryWrites=true&w=majority&appName=chat-mern";
+
+// App Settings
+const PORT = 5000;
+const NODE_ENV = "development";
+const MONGO_MAX_CONNECTION_RETRIES = 5;
+
+// CORS
+const ALLOWED_ORIGINS = "http://localhost:5173,http://localhost:5000";
+const ALLOWED_HEADERS =
+  "Origin, X-Requested-With, Content-Type, Accept, Authorization";
+const ALLOWED_METHODS = "GET, POST, PUT, DELETE, OPTIONS";
+const ALLOW_CREDENTIALS = "true";
+
+// JWT
+const ACCESS_TOKEN_SECRET = "";
+const REFRESH_TOKEN_SECRET = "";
+
+// Google OAuth and Email
+const SD_CHAT_SUPPORT_EMAIL = "sdchatmern@gmail.com";
+const APP_EMAIL = "sdchatmern@gmail.com";
+const APP_EMAIL_PASSWORD = "mqgrtnckzfkenehf";
+
+// Cloudinary
+const CLOUDINARY_CLOUD_NAME = "";
+const CLOUDINARY_API_KEY = "";
+const CLOUDINARY_API_SECRET = "";
---
+++ frontend/env.example.js
@@ -0,0 +1,5 @@
+//! >> Define all environment variables in .env file in local development << !//
+//! >> Supply these all environment variables in production environment << !//
+
+const VITE_BACKEND_URL = "http://localhost:5000/api";
+const VITE_BACKEND_BASE_URL = "http://localhost:5000";
